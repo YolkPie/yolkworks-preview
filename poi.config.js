4b0c0092f4bd5e8fdbe0616580516b88c9361d82
cuimenglin <cuimenglin@jd.com>
2020-02-28 15:40:17 +0800

    chore: 修改homepage

--- poi.config.js
+++ poi.config.js
@@ -35,7 +35,7 @@ module.exports = {
     sourceMap: false
   },
   hash: false,
-  homepage: '/',
+  homepage: './',
   env: Object.assign({
     VERSION: `v${pkg.version}-${repoLatestCommit().commit.slice(0, 7)}`,
     LATEST_COMMIT: repoLatestCommit().commit.slice(0, 7),
